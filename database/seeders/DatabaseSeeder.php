3a5cae414879fc1023c52b7093c685e844f96763
Romulo Sousa <romulosousa27@hotmail.com>
2025-07-21 19:32:28 -0300

    chore: change fews configurations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory()->createMany([
+            ['name' => 'Jonh Doe', 'email' => 'jonh@example.com'],
+            ['name' => 'Jane Doe', 'email' => 'jane@example.com']
         ]);
     }
 }
